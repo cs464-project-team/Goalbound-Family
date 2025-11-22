ca0959f487ef03caba89131ac63bd69af0145c41
Ivyn Yak <ivyn.yak.2022@scis.smu.edu.sg>
2025-11-22 18:14:41 +0800

    config shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   test: {
     environment: "jsdom",
     globals: true,
