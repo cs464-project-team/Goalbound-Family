53f9b5ae35de6f1cdf0f05f2df7883aedc414676
lsd2000 <sdlee.2021@scis.smu.edu.sg>
2025-11-24 20:40:58 +0800

    updated to have lint testing

--- backend/DTOs/UserDto.cs
+++ backend/DTOs/UserDto.cs
@@ -9,7 +9,6 @@ public class UserDto
     public string FirstName { get; set; } = string.Empty;
     public string LastName { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
-    
 }
 
 /// <summary>
